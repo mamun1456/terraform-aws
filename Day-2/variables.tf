4a5fd92175ea2c8a96771ddaa3b4320d9c938ed5
mamun1456 <mamun1456@gmail.com>
2023-07-06 16:20:09 -0400

    created vpc with reusable code using tfvars and variables

---
+++ Day-2/variables.tf
@@ -0,0 +1,16 @@
+variable "region" {}
+
+variable "availability_zones" {}
+
+variable "env" {}
+
+variable "instance_type" {}
+
+variable "vpc_cidr" {}
+
+variable "public_subnet_cidr" {}
+
+variable "private_subnet_cidr" {}
+
+
+
