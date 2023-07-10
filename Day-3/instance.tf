f4a3d72385472623b4b9b54a7880576c5669c519
mamun1456 <mamun1456@gmail.com>
2023-07-10 16:30:51 -0400

    added code for ec2 instance and ssh to it

---
+++ Day-3/instance.tf
@@ -0,0 +1,93 @@
+resource "aws_security_group" "public" {
+  name        = "${var.env}-public"
+  description = "public security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["70.107.176.20/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-public"
+  }
+}
+
+resource "aws_instance" "public" {
+  ami                         = data.aws_ami.amazon_linux.id
+  instance_type               = "t3.micro"
+  subnet_id                   = aws_subnet.public[0].id
+  vpc_security_group_ids      = [aws_security_group.public.id]
+  key_name                    = "ec2"
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "${var.env}-public"
+  }
+
+}
+
+resource "aws_security_group" "private" {
+  name        = "${var.env}-private"
+  description = "private security group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description = "SSH from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.vpc_cidr]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-private"
+  }
+}
+
+resource "aws_instance" "private" {
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = "t3.micro"
+  subnet_id              = aws_subnet.private[0].id
+  vpc_security_group_ids = [aws_security_group.private.id]
+  key_name               = "ec2"
+
+  tags = {
+    Name = "${var.env}-private"
+  }
+
+}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-kernel-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["amazon"]
+
+}
